fa84da6f31cbe00f2a1e7c66e0fb311bd20f72b8
이동준 <69755191+Psychopath7@users.noreply.github.com>
2026-08-03 23:32:47 +0900

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,63 +1,30 @@
-#include <sys/types.h>
-#include "zygisk.hpp"
-#include <android/log.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <thread>
-#include <inttypes.h>
+/* Copyright 2022-2023 John "topjohnwu" Wu
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
 
-#define LOG_TAG "MyZygiskModule"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#include <cstdlib>
+#include <unistd.h>
+#include <fcntl.h>
+#include <android/log.h>
+
+#include "zygisk.hpp"
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
+using zygisk::ServerSpecializeArgs;
 
-// --- 1. /proc/self/maps 읽기 유틸리티 ---
-uintptr_t get_module_base(const char* module_name) {
-    FILE *fp = fopen("/proc/self/maps", "r");
-    if (!fp) return 0; 
-    
-    char line[512];
-    uintptr_t base_addr = 0;
-    while (fgets(line, sizeof(line), fp)) {
-        if (strstr(line, module_name)) {
-            base_addr = (uintptr_t)strtoull(line, NULL, 16);
-            break; 
-        }
-    }
-    fclose(fp);
-    return base_addr;
-}
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "MyModule", __VA_ARGS__)
 
-// --- 2. 비침습적 메모리 감시 스레드 ---
-void monitor_thread() {
-    LOGI("[*] Passive memory monitor thread started...");
-    
-    uintptr_t base_addr = 0;
-    
-    // libil2cpp.so가 메모리에 올랐는지 100ms 간격으로 확인 (최대 30초간 대기)
-    for (int i = 0; i < 300; i++) {
-        base_addr = get_module_base("libil2cpp.so");
-        if (base_addr != 0) {
-            LOGI("[!] ========================================");
-            LOGI("[!] libil2cpp.so Loaded in Memory!");
-            LOGI("[!] Base Address : 0x%" PRIxPTR, base_addr);
-            LOGI("[!] Target Addr  : 0x%" PRIxPTR, base_addr + 0x4463454);
-            LOGI("[!] ========================================");
-            break;
-        }
-        usleep(100000); // 0.1초 대기
-    }
-
-    if (base_addr == 0) {
-        LOGE("[-] Timeout: libil2cpp.so was not loaded in 30 seconds.");
-    }
-}
-
-// --- 3. Zygisk 메인 로직 ---
 class MyModule : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
@@ -65,23 +32,48 @@ public:
         this->env = env;
     }
 
-    void postAppSpecialize(const AppSpecializeArgs *args) override {
-        const char* process_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        
-        // ⚠️ 주의: 반드시 분석할 앱의 실제 패키지명으로 입력하세요!
-        if (process_name != nullptr && strstr(process_name, "com.gear2.growslayer")) {
-            LOGI("[!] Target App Specialized: %s", process_name);
-            
-            // 시스템 메모리를 변조하지 않고, 수동 감시 스레드만 독립 실행
-            std::thread(monitor_thread).detach();
-        }
-        
-        env->ReleaseStringUTFChars(args->nice_name, process_name);
+    void preAppSpecialize(AppSpecializeArgs *args) override {
+        // Use JNI to fetch our process name
+        const char *process = env->GetStringUTFChars(args->nice_name, nullptr);
+        preSpecialize(process);
+        env->ReleaseStringUTFChars(args->nice_name, process);
+    }
+
+    void preServerSpecialize(ServerSpecializeArgs *args) override {
+        preSpecialize("system_server");
     }
 
 private:
     Api *api;
     JNIEnv *env;
+
+    void preSpecialize(const char *process) {
+        // Demonstrate connecting to to companion process
+        // We ask the companion for a random number
+        unsigned r = 0;
+        int fd = api->connectCompanion();
+        read(fd, &r, sizeof(r));
+        close(fd);
+        LOGD("process=[%s], r=[%u]\n", process, r);
+
+        // Since we do not hook any functions, we should let Zygisk dlclose ourselves
+        api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
+    }
+
 };
 
+static int urandom = -1;
+
+static void companion_handler(int i) {
+    if (urandom < 0) {
+        urandom = open("/dev/urandom", O_RDONLY);
+    }
+    unsigned r;
+    read(urandom, &r, sizeof(r));
+    LOGD("companion r=[%u]\n", r);
+    write(i, &r, sizeof(r));
+}
+
+// Register our module class and the companion handler function
 REGISTER_ZYGISK_MODULE(MyModule)
+REGISTER_ZYGISK_COMPANION(companion_handler)
